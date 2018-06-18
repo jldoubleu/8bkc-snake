6a86d52b2c3bcee53163dfb569ae01855350f144
jay doubleu <justin.williams.1181@gmail.com>
2018-06-17 22:12:38 -0500

    White space conversion
    Spaces over tabs

--- main/app_main.c
+++ main/app_main.c
@@ -3,11 +3,11 @@
 #include "tilegfx.h"       // tilegfx functions and types
 
 static void do_powerbtn_menu() {
-	int i = powerbtn_menu_show(tilegfx_get_fb());
-	if (i == POWERBTN_MENU_EXIT) {
+  int i = powerbtn_menu_show(tilegfx_get_fb());
+  if (i == POWERBTN_MENU_EXIT) {
     kchal_exit_to_chooser();
   }
-	if (i == POWERBTN_MENU_POWERDOWN) {
+  if (i == POWERBTN_MENU_POWERDOWN) {
     kchal_power_down();
   }
 }
@@ -15,20 +15,20 @@ static void do_powerbtn_menu() {
 //Get bitmap of keys, but only put an 1 for a key that wasn't pressed the previous call but
 //is pressed now.
 int get_keydown() {
-	static int oldBtns = 0xffff; // <-- static int be careful
-	int newBtns = kchal_get_keys();
-	int ret = (oldBtns ^ newBtns) & newBtns;
-	oldBtns = newBtns;
-	return ret;
+  static int oldBtns = 0xffff; // <-- static int be careful
+  int newBtns = kchal_get_keys();
+  int ret = (oldBtns ^ newBtns) & newBtns;
+  oldBtns = newBtns;
+  return ret;
 }
 
 void app_main() {
-	kchal_init();        //Initialize the PocketSprite SDK.
-	tilegfx_init(1, 50); //Initialize TileGFX, Doublesized mode, 50FPS
-	while (1) {
-		int btn = get_keydown();
-		if (btn & KC_BTN_POWER) {
+  kchal_init();        //Initialize the PocketSprite SDK.
+  tilegfx_init(1, 50); //Initialize TileGFX, Doublesized mode, 50FPS
+  while (1) {
+    int btn = get_keydown();
+    if (btn & KC_BTN_POWER) {
       do_powerbtn_menu();
     }
-	}
+  }
 }
