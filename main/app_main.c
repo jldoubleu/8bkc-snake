b5f374fe858aca63dd721f035bdeca78785745bc
jay doubleu <justin.williams.1181@gmail.com>
2018-06-18 22:17:20 -0500

    Title Screen
    Write the title. Minor introduction into ugui. Drew a string on the
    screen. Centered it. I do not know why I was not able to use any
    font besides 6X8 though.

--- main/app_main.c
+++ main/app_main.c
@@ -1,7 +1,10 @@
+#include <stdint.h>        // unit16_t
+#include <string.h>        // strlen
 #include <stdbool.h>       // false and true def
 #include "8bkc-hal.h"      // kchalinit, etc. includes nvs functions
 #include "8bkc-ugui.h"     // kcugui_cls
 #include "powerbtn_menu.h" // Power Button menu stuff powerbtn_menu_show, constants, etc
+#include "ugui.h"
 
 static void do_powerbtn_menu() {
   int i = powerbtn_menu_show(kcugui_get_fb());
@@ -23,10 +26,22 @@ int get_keydown() {
   return ret;
 }
 
+void draw_title_screen() {
+  char* title = "Snake\n";
+  size_t title_length = strlen(title); 
+  uint8_t font_width = 6;
+  uint16_t offset = (KC_SCREEN_W - (title_length * font_width)) / 2;
+  
+  kcugui_cls();
+  UG_FontSelect(&FONT_6X8);
+  UG_SetForecolor(C_LAWN_GREEN);
+  UG_PutString(offset, 0, title);
+}
+
 void app_main() {
   kchal_init();  // Initialize the PocketSprite SDK.
   kcugui_init(); // Initialize ugui
-  kcugui_cls();  // Initialize framebuffer to black
+  draw_title_screen();
   while (1) {
     int btn = get_keydown();
     if (btn & KC_BTN_POWER) {
